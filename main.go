56d176dd71e9902a8897a7dc3309155877e55c6f
whoisnian <zhuchangbao1998@gmail.com>
2020-12-23 01:14:22 +0800

    ADD: add uploadHandler

--- main.go
+++ main.go
@@ -3,23 +3,38 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/whoisnian/share-Go/pkg/server"
 )
 
 func indexHander(store server.Store) {
-	url := store.URL()
-	if url.Path != "/" {
-		store.Respond404()
-		return
-	}
-	time.Sleep(time.Second * 1)
+	page := `<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="utf-8">
+		<title>Index Page</title>
+	</head>
+	<body>
+	  <h2>Upload a file</h2>
+	  <form action="/upload" method="post" enctype="multipart/form-data">
+		  <input type="file" name="file" multiple>
+		  <br>
+		  <input type="submit" name="submit" value="Submit">
+	  </form>
+	</body>
+</html>`
+	store.Respond200([]byte(page))
+}
+
+// https://golang.org/pkg/mime/multipart/
+func uploadHandler(store server.Store) {
+	store.Save("uploads")
 	store.Respond200(nil)
 }
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/upload", server.MakeHander(uploadHandler))
 	http.Handle("/", server.MakeHander(indexHander))
 
 	log.Printf("Server started: <http://127.0.0.1:9000>\n")
--- pkg/server/request.go
+++ pkg/server/request.go
@@ -1,6 +1,11 @@
 package server
 
-import "net/url"
+import (
+	"io"
+	"net/url"
+	"os"
+	"path"
+)
 
 // CookieValue ...
 func (store Store) CookieValue(name string) string {
@@ -14,4 +19,27 @@ func (store Store) CookieValue(name string) string {
 // URL ...
 func (store Store) URL() *url.URL {
 	return store.r.URL
+}
+
+// Save ...
+func (store Store) Save(dir string) {
+	reader, err := store.r.MultipartReader()
+	if err != nil {
+		return
+	}
+	for {
+		part, err := reader.NextPart()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			return
+		}
+		file, err := os.Create(path.Join(dir, part.FileName()))
+		if err != nil {
+			return
+		}
+		io.Copy(file, part)
+		file.Close()
+	}
 }
