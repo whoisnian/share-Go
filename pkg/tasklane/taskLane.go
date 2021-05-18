93356945844ee70d40f1d99133b6f25ff9179eaa
whoisnian <zhuchangbao1998@gmail.com>
2021-05-19 01:07:15 +0800

    ADD: tasklane

---
+++ pkg/tasklane/taskLane.go
@@ -0,0 +1,89 @@
+package tasklane
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// Multiple TaskQueue => One TaskLane
+// One Buffered Channel + One Blocking Channel + One Goroutine(Worker) => One TaskQueue
+type TaskLane struct {
+	laneSize       int
+	queueSize      int
+	queueList      []chan func()
+	universalQueue chan func()
+	timeout        time.Duration
+}
+
+func (taskLane *TaskLane) startQueue(index int) {
+	for {
+		taskFunc := <-taskLane.queueList[index]
+		select {
+		case taskLane.queueList[index+taskLane.laneSize] <- taskFunc:
+		default:
+			select {
+			case taskLane.queueList[index+taskLane.laneSize] <- taskFunc:
+			case taskLane.universalQueue <- taskFunc:
+			}
+		}
+	}
+}
+
+func (taskLane *TaskLane) startWorker(index int) {
+	var taskFunc func()
+	for {
+		select {
+		case taskFunc = <-taskLane.queueList[index+taskLane.laneSize]:
+		default:
+			select {
+			case taskFunc = <-taskLane.queueList[index+taskLane.laneSize]:
+			case taskFunc = <-taskLane.universalQueue:
+			}
+		}
+		taskFunc()
+	}
+}
+
+func New(laneSize, queueSize int) *TaskLane {
+	// create channels for TaskQueue
+	queueList := make([]chan func(), laneSize*2)
+	for i := 0; i < laneSize; i++ {
+		queueList[i] = make(chan func(), queueSize)
+		queueList[i+laneSize] = make(chan func())
+	}
+	universalQueue := make(chan func())
+
+	// create TaskLane
+	taskLane := &TaskLane{
+		laneSize:       laneSize,
+		queueSize:      queueSize,
+		queueList:      queueList,
+		universalQueue: universalQueue,
+		timeout:        time.Second * 5,
+	}
+
+	// start TaskQueue
+	for i := 0; i < taskLane.laneSize; i++ {
+		go taskLane.startQueue(i)
+		go taskLane.startWorker(i)
+	}
+
+	return taskLane
+}
+
+func (taskLane *TaskLane) ShowStatus() {
+	for i := 0; i < taskLane.laneSize; i++ {
+		fmt.Print(len(taskLane.queueList[i]), " ")
+	}
+	fmt.Print("\n")
+}
+
+func (taskLane *TaskLane) PushTask(taskFunc func(), index int) error {
+	select {
+	case taskLane.queueList[index] <- taskFunc:
+		return nil
+	case <-time.After(time.Second * 5):
+		return errors.New("PushTaskTimeout")
+	}
+}
