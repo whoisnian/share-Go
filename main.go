30156cfbbf5ab9dc5ea25d65fe00bb211a50be3b
whoisnian <zhuchangbao1998@gmail.com>
2021-07-02 15:00:43 +0800

    ADD: add browser visiting tips

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net"
 	"os"
 	"os/signal"
 
@@ -9,11 +10,18 @@ import (
 	"github.com/whoisnian/share-Go/pkg/ftpd"
 	"github.com/whoisnian/share-Go/pkg/httpd"
 	"github.com/whoisnian/share-Go/pkg/logger"
+	"github.com/whoisnian/share-Go/pkg/util"
 )
 
 func main() {
 	config.Init()
 	logger.SetDebug(config.Debug)
+
+	if _, port, err := net.SplitHostPort(config.HTTPListenAddr); err == nil {
+		if ip, err := util.GetOutBoundIP(); err == nil {
+			logger.Info("Try visiting \x1b[32mhttp://", net.JoinHostPort(ip.String(), port), "\x1b[0m in your browser.")
+		}
+	}
 
 	router.Init()
 	go httpd.Start(config.HTTPListenAddr)
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,5 +1,9 @@
 package util
 
+import (
+	"net"
+)
+
 // Contain determines whether a string slice includes a certain value.
 func Contain(slice []string, value string) bool {
 	for _, item := range slice {
@@ -12,10 +16,23 @@ func Contain(slice []string, value string) bool {
 
 // IsSpace determines whether a byte belongs to space.
 func IsSpace(ch byte) bool {
-	return ' ' == ch ||
-		'\n' == ch ||
-		'\r' == ch ||
-		'\t' == ch ||
-		'\v' == ch ||
-		'\f' == ch
+	return ch == ' ' ||
+		ch == '\n' ||
+		ch == '\r' ||
+		ch == '\t' ||
+		ch == '\v' ||
+		ch == '\f'
+}
+
+// GetOutBoundIP get preferred outbound ip of current process.
+// https://stackoverflow.com/a/37382208
+func GetOutBoundIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP, nil
 }
