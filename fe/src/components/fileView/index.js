64d98eaeb632c2d9194030a60e0bcb65da61c302
whoisnian <zhuchangbao1998@gmail.com>
2024-10-01 15:57:22 +0800

    DEL: fe overwriting iframe.body.style

--- fe/src/components/fileView/index.js
+++ fe/src/components/fileView/index.js
@@ -18,18 +18,6 @@ const createFileView = async (oriPath) => {
     style: 'position:fixed;border:none;top:0;bottom:0;left:0;right:0;width:100%;height:100%;'
   })
 
-  const interval = setInterval(() => {
-    if (window.frames['raw'] && window.frames['raw'].document.documentURI === iframe.src) {
-      if (window.frames['raw'].document.body) window.frames['raw'].document.body.style = "margin:0;"
-      clearInterval(interval)
-    }
-  }, 100)
-
-  iframe.onload = () => {
-    if (window.frames['raw'].document.body) window.frames['raw'].document.body.style = "margin:0;"
-    clearInterval(interval)
-  }
-
   return iframe
 }
 
