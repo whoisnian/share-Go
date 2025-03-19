cc6fc2c83e3309d05f6de03cde6fd323eef263a5
whoisnian <zhuchangbao1998@gmail.com>
2025-03-20 02:48:58 +0800

    FIX: fe: esbuild api serve breaking change for dev server

--- fe/scripts/start.js
+++ fe/scripts/start.js
@@ -1,6 +1,6 @@
 const { createServer, request: httpRequest } = require('http')
 const { request: httpsRequest } = require('https')
-const { context: esContext } = require('esbuild')
+const { context } = require('esbuild')
 const { generateHtmlFromTemplate, copyPlugin } = require('./plugin')
 const { fromRoot, fromOutput } = require('./function')
 const { buildConfig } = require('./esbuild.config')
@@ -26,36 +26,45 @@ const proxyTransform = (url) => {
 }
 
 const runMain = async () => {
-  const esCtx = await esContext({
+  const ctx = await context({
     ...buildConfig,
+    write: false,
+    sourcemap: true,
     entryNames: '[name]',
     plugins: [copyPlugin(fromRoot('public'), fromOutput())]
   })
-  await esCtx.watch()
-  const esServeRes = await esCtx.serve({ servedir: fromOutput() })
+  await ctx.watch()
+  const { hosts, port } = await ctx.serve({ servedir: fromOutput() })
 
   createServer((req, res) => {
     if (req.url === '/') {
       res.writeHead(302, { location: '/view/' })
       res.end()
+      console.log(' \x1b[34m[direct]\x1b[0m 302 redirect / to /view/')
       return
     }
-    const url = proxyTransform(req.url)
-    const proxyReq = request(url || `http://${esServeRes.host}:${esServeRes.port}${req.url}`, {
+    const proxyUrl = proxyTransform(req.url)
+    const proxyReq = request(proxyUrl || `http://${hosts[0]}:${port}${req.url}`, {
       method: req.method,
       headers: req.headers
     }, proxyRes => {
-      if (proxyRes.statusCode === 404 && url === null) {
+      if (proxyRes.statusCode === 404 && proxyUrl === null) {
         res.writeHead(200, { 'content-type': 'text/html' })
         res.end(generateHtmlFromTemplate({
           stylesheetList: ['/static/app.css'],
           scriptList: ['/static/app.js']
         }))
+        console.log(`\x1b[32m[esbuild]\x1b[0m 200 index ${req.method} ${req.url}`)
         return
       }
 
       res.writeHead(proxyRes.statusCode, proxyRes.headers)
       proxyRes.pipe(res, { end: true })
+      if (proxyUrl === null) console.log(`\x1b[32m[esbuild]\x1b[0m ${res.statusCode} ok ${req.method} ${req.url}`)
+      else console.log(`  \x1b[35m[proxy]\x1b[0m ${res.statusCode} ok ${req.method} ${proxyUrl}`)
+    }).on('error', err => {
+      res.writeHead(500, { 'content-type': 'text/plain' }).end(err.message)
+      console.error(`  \x1b[31m[error]\x1b[0m 500 error ${req.method} ${proxyUrl || req.url} ${err.message}`)
     })
 
     req.pipe(proxyReq, { end: true })
