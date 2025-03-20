cc8379831bbf5e41956281ccfcf9082a4030a756
whoisnian <zhuchangbao1998@gmail.com>
2025-03-21 01:49:42 +0800

    CHG: fe: use type:module in package.json

--- fe/eslint.config.js
+++ fe/eslint.config.js
@@ -1,9 +1,17 @@
-module.exports = require('neostandard')({
-  env: ['browser'],
-  globals: {
-    __DEBUG__: 'readonly',
-    __PACKAGE_VERSION__: 'readonly'
-  },
-  files: ['src/**/*.js', 'scripts/**/*.js'],
-  ignores: ['dist/**/*']
-})
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+import globals from 'globals'
+
+export default [
+  ...neostandard({
+    env: ['browser'],
+    globals: {
+      __DEBUG__: 'readonly',
+      __PACKAGE_VERSION__: 'readonly'
+    },
+    files: ['src/**/*.js', 'scripts/**/*.js'],
+    ignores: resolveIgnoresFromGitignore()
+  }),
+  {
+    languageOptions: { ecmaVersion: 2025, globals: globals.es2025 }
+  }
+]
